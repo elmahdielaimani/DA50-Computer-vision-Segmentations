a4df77fab475e98591c254ee1cd4ace2fb509675
ayalaarif <larrifaya@gmail.com>
2024-12-29 21:17:59 +0100

    filtrages des images (admistrateur peut voir seulement les images annotees et l'utilisateur peut voir seulemt les images qui sont pas annotees)

--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -1,4 +1,3 @@
-
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormBuilder } from '@angular/forms';
@@ -9,20 +8,17 @@ import { Observable } from 'rxjs';
 })
 export class ImageService {
   baseURL = 'http://localhost:9992/image';
-  headers = new HttpHeaders().set('Content-Type', 'application/json');
 
   constructor(public fb: FormBuilder, private http: HttpClient) { }
 
-  // Fonction pour obtenir les images avec pagination
-  
-  getImages(page: number = 1, limit: number = 10) {
-    return this.http.get(`${this.baseURL}?page=${page}&limit=${limit}`);
+  // Fonction pour obtenir les images avec pagination et rôle
+  getImagesByRole(page: number = 1, limit: number = 10, role: string): Observable<any> {
+    const headers = new HttpHeaders().set('role', role); // Ajouter le rôle dans les headers
+    return this.http.get(`${this.baseURL}?page=${page}&limit=${limit}`, { headers });
   }
-  
 
   // Fonction pour obtenir une image par son ID
   getImageById(id: string): Observable<any> {
-    return this.http.get(`${this.baseURL}/${id}`, { headers: this.headers });
+    return this.http.get(`${this.baseURL}/${id}`);
   }
 }
-
